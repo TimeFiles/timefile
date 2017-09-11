944e81753b255c24796cb3d3eebaf93d14d18bc5
tiemfiles <578530825@qq.com>
2017-09-11 16:37:22 +0800

    del

--- ZZQRCode/ZZQRCodeFinallyViewController.m
+++ ZZQRCode/ZZQRCodeFinallyViewController.m
@@ -17,7 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"扫描结果";
-    // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 0, SCREEN_WIDTH -40, SCREEN_HEIGHT -64)];
     label.text = self.titleLabelStr;
@@ -28,15 +27,5 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
